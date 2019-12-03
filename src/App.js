ed86f7d3a14b5337710e84067fbc001f4d313314
Zach Pfeifer <mr.zman1@yahoo.com>
2019-12-03 16:26:06 -0700

    AddCard

--- src/App.js
+++ src/App.js
@@ -11,25 +11,33 @@ class App extends Component {
         id: 1,
         name: "player 1",
         initiative: 20,
+        hitpoints: 50,
       }, {
         id: 2,
         name: "player 2",
         initiative: 19,
+        hitpoints: 50,
 
       }, {
         id: 3,
         name: "player 3",
         initiative: 18,
+        hitpoints: 50,
 
       }, {
         id: 4,
         name: "player 4",
         initiative: 17,
+        hitpoints: 50,
 
       }]
     };
     this.updateName = this.updateName.bind(this)
     this.updateInitiative = this.updateInitiative.bind(this)
+    this.updateHitpoints = this.updateHitpoints.bind(this)
+    this.addCard = this.addCard.bind(this)
+
+
 
   }
 
@@ -48,9 +56,29 @@ class App extends Component {
     const index = elements.findIndex(el => el.id === id)
     elements[index].initiative = Number(value);
     this.setState({ elements });
-    this.timeout_ = setTimeout(() => this.sortElements, 500)
+    this.timeout_ = setTimeout(() => this.sortElements(), 500)
   }
 
+  updateHitpoints(id, e) {
+    const { value } = e.target;
+    const elements = this.state.elements
+    const index = elements.findIndex(el => el.id === id)
+    elements[index].hitpoints = Number(value);
+    this.setState({ elements });
+  }
+
+  addCard() {
+    const { elements } = this.state;
+    elements[elements.length] = {
+      id: elements.length + 1,
+      name: `player ${elements.length + 1}`,
+      initiative: -100,
+      hitpoints: 10,
+    };
+    this.setState({
+      elements: elements.sort((l, r) => r.initiative - l.initiative)
+    })
+  }
 
   sortElements() {
     const { elements } = this.state;
@@ -64,13 +92,16 @@ class App extends Component {
     const { elements } = this.state;
     return (
       <div>
+        <button onClick={this.addCard}>+</button>
         {elements.map(elements =>
           <Card
             key={elements.key}
             name={elements.name}
             initiative={elements.initiative}
+            hitpoints={elements.hitpoints}
             onNameChange={this.updateName}
             onInitiativeChange={this.updateInitiative}
+            onHitpointsChange={this.updateHitpoints}
             id={elements.id}
           />
 
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 
-export default ({ id, name, initiative, onNameChange, onInitiativeChange }) =>
+export default ({
+  id,
+  name,
+  initiative,
+  hitpoints,
+  onNameChange,
+  onInitiativeChange,
+  onHitpointsChange }) =>
   <div className="card">
+    <label htmlFor="name">
+      Name:
     <input type="text" value={name} onChange={e => onNameChange(id, e)} />
-    <input type="number" value={initiative} onChange={e => onInitiativeChange(id, e)} />
+    </label>
+    <label htmlFor="initiative">
+      Initiative:
+      <input type="number" value={initiative} onChange={e => onInitiativeChange(id, e)} />
+    </label>
+    <label htmlFor="hitpoints">
+      HP:
+      <input type="number" value={hitpoints} onChange={e => onHitpointsChange(id, e)} />
+    </label>
+
 
   </div>;
