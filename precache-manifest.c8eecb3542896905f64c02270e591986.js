b2d13b8846a7b21bb49dd9fcdaf521ab3b0d846b
Zach Pfeifer <mr.zman1@yahoo.com>
2019-12-05 15:55:32 -0700

    Updates

---
+++ precache-manifest.c8eecb3542896905f64c02270e591986.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e16bcc27b22d2b613644a2c162c6e6",
+    "url": "/Pathfinder-Init/index.html"
+  },
+  {
+    "revision": "fc46f110e0a8a3eb1229",
+    "url": "/Pathfinder-Init/static/css/2.8a82041c.chunk.css"
+  },
+  {
+    "revision": "076a37df98a1a2f22845",
+    "url": "/Pathfinder-Init/static/css/main.82e1702f.chunk.css"
+  },
+  {
+    "revision": "fc46f110e0a8a3eb1229",
+    "url": "/Pathfinder-Init/static/js/2.65291be0.chunk.js"
+  },
+  {
+    "revision": "076a37df98a1a2f22845",
+    "url": "/Pathfinder-Init/static/js/main.cf28ffdd.chunk.js"
+  },
+  {
+    "revision": "a957aef2d3d6c039eed7",
+    "url": "/Pathfinder-Init/static/js/runtime-main.ddc041a2.js"
+  },
+  {
+    "revision": "bc54396771c64863193f7a3fe93d9cff",
+    "url": "/Pathfinder-Init/static/media/rpgawesome-webfont.bc543967.ttf"
+  },
+  {
+    "revision": "c243adb601cb0e965344851b1c46194f",
+    "url": "/Pathfinder-Init/static/media/rpgawesome-webfont.c243adb6.svg"
+  },
+  {
+    "revision": "cbfd0298f008558557418d19abfc2f42",
+    "url": "/Pathfinder-Init/static/media/rpgawesome-webfont.cbfd0298.eot"
+  },
+  {
+    "revision": "e31a701aa9b88125d36119acb8a10482",
+    "url": "/Pathfinder-Init/static/media/rpgawesome-webfont.e31a701a.woff"
+  }
+]);
